ec46afdb379e73d6d123d8ee29b9eb778f6bdeab
Vikash pandit <vikash.pandit0444@gmail.com>
2026-04-20 22:35:20 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,16 @@
+
+
+import Users from "./Users";
+
 function App() {
-      function callFun() {
-    alert("Function called")
-         }
-    return(
-        <>
-        <h1>Anil Sidhu Todos</h1>
-
-      <img
-        src="https://st5.depositphotos.com/1703608/69112/i/450/depositphotos_691120514-stock-photo-adorable-labrador-puppy-closeup-isolated.jpg"
-        alt="dog"
-        className="photo"
-      />
-
-      <ul>
-        <li>Invent new traffic light</li>
-        <li>Rehearse a movie scene</li>
-        <li>Improve the spectrum technology</li>
-      </ul>
-
-      <button onClick={callFun}>Click Me</button>
-         
-        </>
-     
-    )
+  // let name = "Vikash Pandit"
+  return (
+    <div>
+      <h1>Hello</h1>
+      {/* <h2>{name}</h2> */}
+      <Users name="Vikash Pandit" age={23} email="vikash@121234"/>
+    </div>
+  )
 }
-
-
 
 export default App;
---
+++ src/Users.jsx
@@ -0,0 +1,12 @@
+function Users(name,age,email) {
+    return (
+    <div>
+        <h1>User components</h1>
+        <h2>Name: {name}</h2>
+        <h2>Age: {age}</h2>
+        <h3>Email: {email}</h3>
+    </div>
+    )
+}
+
+export default Users;
