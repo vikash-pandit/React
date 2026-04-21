3d2eff1ac7a514a79301b211f18790b122ce06ae
Vikash pandit <vikash.pandit0444@gmail.com>
2026-04-21 21:42:16 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@
 import Users from "./Users";
 
 function App() {
-  // let name = "Vikash Pandit"
+  let userName = "Vikash Pandit"
+  let age = 34;
+  let email = "vikash@1234.com"
   return (
     <div>
       <h1>Hello</h1>
       {/* <h2>{name}</h2> */}
-      <Users name="Vikash Pandit" age={23} email="vikash@121234"/>
+      <Users name = {userName}  age={age} email={email}/>
     </div>
   )
 }
